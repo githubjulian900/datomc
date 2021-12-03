5206ca90975b378fb6c412119fa9d521ea2bc931
githubjulian900 <65156876+githubjulian900@users.noreply.github.com>
2021-12-03 01:21:42 +0100

    a

--- config.sample.js
+++ config.sample.js
@@ -14,7 +14,7 @@ module.exports = {
 		expressSessionPassword: "XXXXXXXXXXX", // Express session password (it can be what you want)
 		failureURL: "http://.maxhgamingc1.biz" // url on which users will be redirected if they click the cancel button (discord authentication)
 	},
-	mongoDB: "mongodb://localhost:27017/AtlantaBot", // The URl of the mongodb database
+	mongoDB: "mongodb+srv://1JULIANSMIDT:<password>@cluster0.gnxhs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", // The URl of the mongodb database
 	prefix: "*", // The default prefix for the bot
 	/* For the embeds (embeded messages) */
 	embed: {
