8fecc1a1a8c741c20c3548ca2d6530c3339bf337
githubjulian900 <65156876+githubjulian900@users.noreply.github.com>
2021-12-04 14:28:23 +0100

    a

--- base/Atlanta.js
+++ base/Atlanta.js
@@ -40,10 +40,7 @@ class Atlanta extends Client {
 		this.logger = require("../helpers/logger"); // Load the logger file
 		this.wait = util.promisify(setTimeout); // client.wait(1000) - Wait 1 second
 		this.functions = require("../helpers/functions"); // Load the functions file
-		this.guildsData = require("../base/Guild"); // Guild mongoose model
-		this.usersData = require("../base/User"); // User mongoose model
-		this.membersData = require("../base/Member"); // Member mongoose model
-		this.logs = require("../base/Log"); // Log mongoose model
+
 		this.dashboard = require("../dashboard/app"); // Dashboard app
 		this.queues = new Collection(); // This collection will be used for the music
 		this.states = {}; // Used for the dashboard
